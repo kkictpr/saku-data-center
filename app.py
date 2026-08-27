c1b1a704a059fdd5a5019aeae9cd2db441300240
kkictpr <77109078+kkictpr@users.noreply.github.com>
2026-08-27 15:42:07 +0700

    ๋j15

--- app.py
+++ app.py
@@ -2287,7 +2287,7 @@ elif menu == "🍦 ร้านไอศกรีม (Ice Cream)":
         st.title("🛒 หน้าจอขายสินค้า (POS Dashboard - Loyverse Style)")
         
         conn = sqlite3.connect('datacenter.db', timeout=15)
-        conn.execute('''CREATE TABLE IF NOT EXISTS pos_cart_temp (
+        conn.execute('''CREATE TABLE IF NOT EXISTS pos_cart_temp (๗
             item_id TEXT PRIMARY KEY,
             name TEXT,
             price REAL,
